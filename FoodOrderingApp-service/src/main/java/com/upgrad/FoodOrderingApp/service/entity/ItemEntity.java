2fa89832265568a44a5c05852108875b82d17717
SudhaAnbalagan <sudhapriya2796@gmail.com>
2021-02-07 17:50:54 +0530

    ''

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/ItemEntity.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/ItemEntity.java
@@ -9,6 +9,28 @@ import java.io.Serializable;
 @SuppressWarnings("ALL")
 @Entity
 @Table(name = "item", schema = "public",catalog = "restaurantdb")
+@NamedQueries(
+        {        @NamedQuery(name = "getItemsByUuid", query = "select i from ItemEntity i where i.uuid =:uuid"),
+
+                @NamedQuery(name = "getAllItems", query = "select i from ItemEntity i "),
+                @NamedQuery(name = "itemByItemId", query = "select i from ItemEntity i where i.id =:id"),
+        }
+)
+
+@NamedNativeQueries({
+        // Using native query as named queries do not support LIMIT in nested statements.
+        @NamedNativeQuery(
+                name = "topFivePopularItemsByRestaurant",
+                query =
+                        "select * from item where id in "
+                                + "(select item_id from order_item where order_id in "
+                                + "(select id from orders where restaurant_id = ? ) "
+                                + "group by order_item.item_id "
+                                + "order by (count(order_item.order_id)) "
+                                + "desc LIMIT 5)",
+                resultClass = ItemEntity.class)
+})
+
 public class ItemEntity implements Serializable {
 
   @Id
