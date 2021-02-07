9ece130ae08b9151ab717849133ddf3729702b80
SudhaAnbalagan <sudhapriya2796@gmail.com>
2021-02-07 18:05:06 +0530

    Added CategoryDao.class

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/CategoryDao.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/CategoryDao.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 @SuppressWarnings("all")
@@ -20,6 +21,16 @@ public class CategoryDao {
         try {
             CategoryEntity categoryEntity = entityManager.createNamedQuery("getCategoryByUuid",CategoryEntity.class).setParameter("uuid",uuid).getSingleResult();
             return categoryEntity;
+        }catch (NoResultException nre){
+            return null;
+        }
+    }
+
+    //To get list categories  from the db if no result it returns null.
+    public List<CategoryEntity> getAllCategoriesOrderedByName() {
+        try {
+            List<CategoryEntity> categoryEntities = entityManager.createNamedQuery("getAllCategoriesOrderedByName",CategoryEntity.class).getResultList();
+            return categoryEntities;
         }catch (NoResultException nre){
             return null;
         }
